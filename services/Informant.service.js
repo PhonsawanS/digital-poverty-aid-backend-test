afa8e53db77c3c5274d925193aeedf7b9fcc428e
Jintan29 <99947622+Jintan29@users.noreply.github.com>
2024-09-14 16:34:50 +0700

    Load form Relation

--- services/Informant.service.js
+++ services/Informant.service.js
@@ -1,11 +1,12 @@
 const { where } = require("sequelize");
 const db = require("../models");
 const informant_model = db.Informant;
+const form_model = db.Form;
 
 
 exports.getInformant = () => {
     try {
-        return informant_model.findAll();
+        return informant_model.findAll({ include: form_model });
       } catch (err) {
         return err;
       }
