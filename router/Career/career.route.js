9a30e2f2a0fd0c7611d9199f91fe0442e730acbd
Jintan29 <99947622+Jintan29@users.noreply.github.com>
2025-01-25 21:13:41 +0700

    add middle ware

--- router/Career/career.route.js
+++ router/Career/career.route.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router()
 const CareerController = require('../../controllers/Career.controller')
+const { auth, allowRole } = require('../../middleware/auth')
 
 router 
     .get('/list',CareerController.list)
-    .post('/create',CareerController.create)
+    .post('/create',auth,CareerController.create)
 
     module.exports = router
--- router/MemberFinancial/memberFinancial.route.js
+++ router/MemberFinancial/memberFinancial.route.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const router = express.Router()
+const { auth, allowRole } = require('../../middleware/auth')
 const MemberFinancialController = require('../../controllers/MemberFinancial.controller')
 
 router
     .get('/list',MemberFinancialController.list)
-    .post('/create',MemberFinancialController.create)
+    .post('/create',auth,MemberFinancialController.create)
+    .get('/LastFinancial/:id',MemberFinancialController.getLastFinancial)
 
 module.exports = router
