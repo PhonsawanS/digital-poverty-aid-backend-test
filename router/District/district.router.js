cf079dac30f2d77ae816d4b88209e710fca25161
Jintan29 <99947622+Jintan29@users.noreply.github.com>
2025-02-26 17:21:14 +0700

    add auth to route fetch data

--- router/District/district.router.js
+++ router/District/district.router.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const districtController = require('../../controllers/district.controller'); // Path แก้ไข
+const { auth, allowRole } = require('../../middleware/auth');
+
 
 router
     .get("/fetch", districtController.fetchDistrictData)
     .get("/get", districtController.getDistrictData)
     .get("/getlatest", districtController.getDatalatest)
-    .post("/fetch-data", districtController.fetchDataFromApi);
+    .post("/fetch-data",auth,allowRole(['superAdmin']), districtController.fetchDataFromApi);
 
 module.exports = router;
